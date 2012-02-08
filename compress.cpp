38d3eaf34ca755a77b874ddac838ce6b1124896f
Danny Goodman <goodman.danny@gmail.com>
2012-02-08 15:58:13 -0800

    read_data works

--- compress.cpp
+++ compress.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -16,12 +19,62 @@ public:
 };
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+vector<int> read_data(string filename) {
+    ifstream data_file;
+    string data_line;
+    vector<int> data;
+    
+    data_file.open(filename.c_str());
+    
+    int x, y, L, valCnt, lineCnt;
+    string pHval;
+    L = 0;
+    lineCnt = 0;
+    while (getline(data_file, data_line)) {
+        lineCnt++;
+        stringstream line_stream(data_line);
+        line_stream >> x >> y;
+        valCnt = 0;
+        while (getline(line_stream, pHval, '\t')) {
+            data.push_back(atoi(pHval.c_str()));
+            valCnt++;
+        }
+        if (L > 0 && L != valCnt) {
+            printf("ERROR: wrong number of values on this line! Got %d values, expected %d values on line %d\n", valCnt, L, lineCnt);
+            exit(1);
+        }
+        L = valCnt;
+    }
+    data_file.close();
+    
+    data.insert(data.begin(), L);
+    data.insert(data.begin(), y);
+    data.insert(data.begin(), x);    
+    
+    printf("Read in %ld pH values with (x, y, L) = (%d, %d, %d)\n", data.size() - 3, x, y, L);
+    
+    return data;
+}
+
 int main() {
     DATCompression *dat = new DATCompression();
     
     dat->init();
     
-    vector<int> data(110*110*75);
+    // vector<int> data(110*110*75);
+    vector<int> data = read_data("data/B28-39_100_100_acq_0007.tab");
     for (int i=0; i<data.size(); ++i) {
         if (data[i] < 0 || data[i] > 16383) {
             cout << "ERROR: initial data value is outside of [0, 16383] range!" << endl;
